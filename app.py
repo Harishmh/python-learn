978f53cb32d1d8c6e9017534456ef465ba8078c7
Harish <c.harish@hotmail.com>
2019-05-30 12:08:44 +0530

    off commmit

--- app.py
+++ app.py
@@ -9,3 +9,16 @@ print('harish mh')
 
 
 print(42 == 42)
+
+
+
+#name = input("whats your name?")
+#color= input("What's your fav colour")
+
+#print(name +' likes '+ color)
+
+birth_year = input('enter your birth year: ')
+age = 2019-int(birth_year)
+print(age)
+print(type(age))
+print(type(birth_year))
